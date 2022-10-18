8e0d911633c49d9ff4ae596e3eaf1f8cad4e1069
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2022-10-18 17:14:21 +0200

    feat: add Sentinel2-L2A driver to handle multi-files requests

--- rest_api/raster2zarr.py
+++ rest_api/raster2zarr.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-import re
-from datetime import datetime
-from zipfile import ZipFile
+from typing import List
 
-from dateutil import parser
+import xarray as xr
 from flask_restx import Namespace, Resource, fields
-from lxml import etree
 
+from utils.drivers.fileFormats import FileFormats
+from utils.drivers.sentinel2_level2A import Sentinel2_Level2A
 from utils.geometry import bbox2polygon
-from utils.raster2zarr import convert
 
 api = Namespace("raster2zarr",
                 description="Transform a raster file into a zarr file")
@@ -17,10 +15,12 @@ api = Namespace("raster2zarr",
 CONVERTRASTER_MODEL = api.model(
     "ConvertRaster",
     {
-        "rasterFile": fields.String(
+        "rasterFile": fields.List(
+            fields.String,
             required=True,
             readonly=True,
-            description="The path to the raster file to convert"),
+            description="The path to the raster files to convert \
+                        in the format TYPE_OF_FILE:PATH"),
         "zarrFile": fields.String(
             required=True,
             readonly=True,
@@ -31,7 +31,11 @@ CONVERTRASTER_MODEL = api.model(
         "bands": fields.List(
             fields.String,
             readonly=True,
-            description="The list of bands to extract")
+            description="The list of bands to extract"),
+        "targetResolution": fields.Integer(
+            readonly=True,
+            description="The requested end resolution in meters"
+        )
     }
 )
 
@@ -44,47 +48,54 @@ PRODUCT_STOP_TIME = "n1:General_Info/Product_Info/PRODUCT_STOP_TIME"
 class ConvertRaster(Resource):
 
     @api.doc(params={
-        'rasterFile': 'The raster file to transform (Sentinel-2 zip)',
+        'rasterFile': 'The raster files to transform (Sentinel-2 zip) \
+            in the format TYPE_OF_FILE:PATH',
         'zarrFile': 'The output of the transformation',
         'roi': 'The Region Of Interest (bbox) to extract',
-        'bands': 'The list of bands to extract'
+        'bands': 'The list of bands to extract',
+        'targetResolution': 'The requested end resolution in meters'
         })
     @api.expect(CONVERTRASTER_MODEL)
     def post(self):
-        rasterFile = api.payload["rasterFile"]
+        rasterFiles: List[str] = api.payload["rasterFile"]
 
-        if rasterFile[-4:] != ".zip":
-            return "Wrong file format", 500
+        polygon = bbox2polygon(api.payload["roi"]) \
+            if "roi" in api.payload \
+            else None
 
-        bandsToExtract = []
-        with ZipFile(rasterFile, "r") as zipObj:
-            listOfFileNames = zipObj.namelist()
-            # Extract timestamp of production of the product
-            for fileName in listOfFileNames:
-                if re.match(r".*MTD_MSI.*\.xml", fileName):
-                    zipObj.extract(fileName, ZIP_EXTRACT_PATH)
-                    metadata: etree._ElementTree = etree.parse(
-                        ZIP_EXTRACT_PATH + fileName)
-                    root: etree._Element = metadata.getroot()
-                    startTime = parser.parse(root.xpath(
-                        PRODUCT_START_TIME, namespaces=root.nsmap)[0].text)
+        targetResolution = api.payload["targetResolution"] \
+            if "targetResolution" in api.payload \
+            else 10
 
-                    stopTime = parser.parse(root.xpath(
-                        PRODUCT_STOP_TIME, namespaces=root.nsmap)[0].text)
+        datasets = []
 
-                    productTime = int((datetime.timestamp(startTime)
-                                       + datetime.timestamp(stopTime)) / 2)
+        for idx, rasterFile in enumerate(rasterFiles):
+            try:
+                fileFormat, rasterPath = rasterFile.split(":", maxsplit=1)
+            except ValueError:
+                return "'rasterFile' should be a list of TYPE_OF_FILE:PATH", \
+                        500
 
-            for band in api.payload["bands"]:
-                for fileName in listOfFileNames:
-                    if re.match(r".*/IMG_DATA/.*" + band + r"\.jp2", fileName):
-                        zipObj.extract(fileName, ZIP_EXTRACT_PATH)
-                        bandsToExtract.append(ZIP_EXTRACT_PATH + fileName)
+            if fileFormat == FileFormats.SENTINEL2_2A.value:
+                rasterArchive = Sentinel2_Level2A(rasterPath,
+                                                  api.payload["bands"],
+                                                  targetResolution)
+            else:
+                return f"'{fileFormat}' format not accepted", 500
 
-        try:
-            polygon = bbox2polygon(api.payload["roi"])
-            convert(bandsToExtract, api.payload["zarrFile"],
-                    productTime, polygon=polygon)
-            return "Operation completed", 200
-        except Exception:
-            return "Error in the process", 500
+            try:
+                dataset = rasterArchive.convert(
+                    api.payload["zarrFile"] + f"_{idx}", polygon=polygon)
+                datasets.append(dataset)
+            except Exception:
+                return f"Error when generating the ZARR for {rasterFile}", 500
+
+        if len(datasets) != 1:
+            print("Combining datasets")
+            try:
+                xr.combine_by_coords(datasets) \
+                    .to_zarr(api.payload["zarrFile"], mode="w")
+            except Exception:
+                return "Error when merging the intermediary files", 500
+
+        return "Operation completed", 200
---
+++ utils/drivers/sentinel2_level2A.py
@@ -0,0 +1,123 @@
+import re
+from datetime import datetime
+from zipfile import ZipFile
+
+from dateutil import parser
+from lxml import etree
+
+from .abstractRasterArchive import AbstractRasterArchive
+
+PRODUCT_START_TIME = "n1:General_Info/Product_Info/PRODUCT_START_TIME"
+PRODUCT_STOP_TIME = "n1:General_Info/Product_Info/PRODUCT_STOP_TIME"
+
+# Different resolutions in meters of the Sentinel2 Level2A files
+HIGH_RESOLUTION = 10
+MED_RESOLUTION = 20
+LOW_RESOLUTION = 60
+
+
+class Sentinel2_Level2A(AbstractRasterArchive):
+
+    def __init__(self, rasterPath, bands,
+                 targetResolution, zipExtractPath="tmp/"):
+
+        self._findBandsResolution(bands, targetResolution)
+        self._extract_metadata(rasterPath, bands, zipExtractPath)
+
+    def _findBandsResolution(self, bands, targetResolution):
+        # Force the resolution to be higher than HIGH_RESOLUTION
+        self.targetResolution = max(HIGH_RESOLUTION, targetResolution)
+        # Force the resolution to be at least of the most precise band
+        if "B08" in bands:
+            self.targetResolution = HIGH_RESOLUTION
+
+        self.bandsWithResolution = {}
+        for band in bands:
+            if band == "AOT":
+                self._bandAvailableEveryResolution(band)
+            elif band == "B01":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "B02":
+                self._bandAvailableEveryResolution(band)
+            elif band == "B03":
+                self._bandAvailableEveryResolution(band)
+            elif band == "B04":
+                self._bandAvailableEveryResolution(band)
+            elif band == "B05":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "B06":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "B07":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "B08":
+                self.bandsWithResolution[band] = HIGH_RESOLUTION
+            elif band == "B8A":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "B09":
+                self.bandsWithResolution[band] = LOW_RESOLUTION
+            elif band == "B10":
+                raise FileNotFoundError("Band B10 does not exist \
+                                        in Sentinel2-L2A files")
+            elif band == "B11":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "B12":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "SCL":
+                self._bandAvailableMedAndLowResolution(band)
+            elif band == "TCI":
+                self._bandAvailableEveryResolution(band)
+            elif band == "WVP":
+                self._bandAvailableEveryResolution(band)
+
+        # targetResolution can not be higher than the resolutions of the bands
+        self.targetResolution = min(self.targetResolution,
+                                    min(self.bandsWithResolution.values()))
+
+    def _bandAvailableEveryResolution(self, band):
+        if HIGH_RESOLUTION <= self.targetResolution < MED_RESOLUTION:
+            self.bandsWithResolution[band] = HIGH_RESOLUTION
+        elif self.targetResolution < LOW_RESOLUTION:
+            self.bandsWithResolution[band] = MED_RESOLUTION
+        else:
+            self.bandsWithResolution[band] = LOW_RESOLUTION
+
+    def _bandAvailableMedAndLowResolution(self, band):
+        if self.targetResolution < LOW_RESOLUTION:
+            self.bandsWithResolution[band] = MED_RESOLUTION
+        else:
+            self.bandsWithResolution[band] = LOW_RESOLUTION
+
+    def _extract_metadata(self, rasterPath, bands, zipExtractPath):
+        self.bandsToExtract = {}
+
+        if rasterPath[-4:] != ".zip":
+            raise FileNotFoundError("File does not have the expected format")
+
+        with ZipFile(rasterPath, "r") as zipObj:
+            listOfFileNames = zipObj.namelist()
+            # Extract timestamp of production of the product
+            for fileName in listOfFileNames:
+                if re.match(r".*MTD_MSI.*\.xml", fileName):
+                    zipObj.extract(fileName, zipExtractPath)
+                    metadata: etree._ElementTree = etree.parse(
+                        zipExtractPath + fileName)
+                    root: etree._Element = metadata.getroot()
+                    startTime = parser.parse(root.xpath(
+                        PRODUCT_START_TIME, namespaces=root.nsmap)[0].text)
+
+                    endTime = parser.parse(root.xpath(
+                        PRODUCT_STOP_TIME, namespaces=root.nsmap)[0].text)
+
+                    self.productTime = int((datetime.timestamp(startTime)
+                                            + datetime.timestamp(endTime)) / 2)
+
+            for band in bands:
+                bandResolution = self.bandsWithResolution[band]
+                for fileName in listOfFileNames:
+                    if re.match(rf".*/IMG_DATA/R{bandResolution}m/" +
+                                rf".*{band}_{bandResolution}m\.jp2",
+                                fileName):
+
+                        print(fileName)
+                        zipObj.extract(fileName, zipExtractPath)
+                        self.bandsToExtract[band] = zipExtractPath + fileName
